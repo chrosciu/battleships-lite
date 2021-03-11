47917dcc57db3086315844c1754668ace5cf91b7
Marcin Chrost <marcin.chrost@contractors.roche.com>
2021-03-11 12:19:15 +0100

    Initial revision

---
+++ src/main/java/com/chrosciu/Game.java
@@ -0,0 +1,37 @@
+package com.chrosciu;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        //create empty ship list...
+        List<Triple<Pair<Integer, Integer>, Integer, Boolean>> rv = new ArrayList<>();
+        //... and fill it with ships placed on board
+        rv.add(Triple.of(Pair.of(1, 1), 4, true));
+        rv.add(Triple.of(Pair.of(6, 7), 2, false));
+        //let's start the game
+        Shooter shooter = new Shooter(rv);
+        Scanner keyboard = new Scanner(System.in);
+        //read user shots
+        for (;;) {
+            //read field coordinates...
+            System.out.println("enter a");
+            int a = keyboard.nextInt();
+            System.out.println("enter b");
+            int b = keyboard.nextInt();
+            //... and take shot !
+            int r = shooter.shoot(Pair.of(a, b));
+            System.out.println(r);
+            //if all ships sunk finish the game
+            if (3 == r) {
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/chrosciu/Shooter.java
@@ -0,0 +1,76 @@
+package com.chrosciu;
+
+import org.apache.commons.lang3.tuple.MutableTriple;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shooter {
+
+    private List<List<MutableTriple<Integer, Integer, Boolean>>> data = new ArrayList<>();
+
+    /**
+     * Initialize shooter with given list of ships on board
+     *
+     * @param input - list of ships. Each ship is described by first field coordinate, length and orientation
+     *              (true - vertical, false - horizontal)
+     */
+    public Shooter(List<Triple<Pair<Integer, Integer>, Integer, Boolean>> input) {
+        for (int i = 0; i < input.size(); ++i) {
+            List<MutableTriple<Integer, Integer, Boolean>> list = new ArrayList<>();
+            for (int j = 0; j < input.get(i).getMiddle(); ++j) {
+                if (input.get(i).getRight()) {
+                    list.add(MutableTriple.of(input.get(i).getLeft().getLeft(), input.get(i).getLeft().getRight() + j, false));
+                } else {
+                    list.add(MutableTriple.of(input.get(i).getLeft().getLeft() + j, input.get(i).getLeft().getRight(), false));
+                }
+            }
+            data.add(list);
+        }
+    }
+
+    /**
+     * Take shot for given field and return shot result
+     *
+     * @param s - field coordinates
+     * @return - shot result: 0 - no hit, 1 - ship hit, 2 - ship sunk, 3 - all ships sunk
+     */
+    public int shoot(Pair<Integer, Integer> s) {
+        int rv = 0;
+        //iterate through all ships
+        for (int i = 0; i < data.size() && 0 == rv; ++i) {
+            //iterate through all ship fields
+            for (int j = 0; j < data.get(i).size() && 0 == rv; ++j) {
+                //if any of ship fields is equal to passed field - mark as hit
+                if (data.get(i).get(j).getLeft().equals(s.getLeft()) && data.get(i).get(j).getMiddle().equals(s.getRight())) {
+                    data.get(i).get(j).setRight(true);
+                    rv = 1;
+                }
+            }
+            //if ship is hit - check if it is sunk
+            if (1 == rv) {
+                //iterate through all fields and check if they are all hit
+                boolean a = true;
+                for (int j = 0; j < data.get(i).size() && a; ++j) {
+                    a &= data.get(i).get(j).getRight();
+                }
+                if (a) {
+                    rv = 2;
+                }
+            }
+        }
+        //check if all ships are sunk
+        boolean a = true;
+        for (int i = 0; i < data.size() && a; ++i) {
+            for (int j = 0; j < data.get(i).size() && a; ++j) {
+                a &= data.get(i).get(j).getRight();
+            }
+        }
+        if (a) {
+            rv = 3;
+        }
+        return rv;
+    }
+}
