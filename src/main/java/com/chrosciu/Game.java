be74a0c02572b39a7c2871b432ff8ada2d3c856d
Marcin Chrost <marcin.chrost@contractors.roche.com>
2021-03-13 12:12:01 +0100

    Field class used instead of Pair

--- src/main/java/com/chrosciu/Game.java
+++ src/main/java/com/chrosciu/Game.java
@@ -1,32 +1,30 @@
 package com.chrosciu;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
         //create empty ship list...
-        List<Triple<Pair<Integer, Integer>, Integer, Boolean>> rv = new ArrayList<>();
+        List<Triple<Field, Integer, Boolean>> rv = new ArrayList<>();
         //... and fill it with ships placed on board
-        rv.add(Triple.of(Pair.of(1, 1), 4, true));
-        rv.add(Triple.of(Pair.of(6, 7), 2, false));
+        rv.add(Triple.of(Field.of(1, 1), 4, true));
+        rv.add(Triple.of(Field.of(6, 7), 2, false));
         //let's start the game
         Shooter shooter = new Shooter(rv);
         Scanner keyboard = new Scanner(System.in);
         //read user shots
-        for (;;) {
-            //read field coordinates...
-            System.out.println("enter a");
-            int a = keyboard.nextInt();
-            System.out.println("enter b");
-            int b = keyboard.nextInt();
+        while (true) {
+            System.out.println("enter field coordinates:");
+            System.out.println("enter x coordinate: ");
+            int xCoordinate = keyboard.nextInt();
+            System.out.println("enter y coordinate: ");
+            int yCoordinate = keyboard.nextInt();
             //... and take shot !
-            int r = shooter.shoot(Pair.of(a, b));
+            int r = shooter.shoot(Field.of(xCoordinate, yCoordinate));
             System.out.println(r);
             //if all ships sunk finish the game
             if (3 == r) {
