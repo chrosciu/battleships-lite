1c22e8aeb313291e96aef7b02a49e8ccf7e29c22
Marcin Chrost <mchrost82@gmail.com>
2022-12-03 14:44:24 +0100

    Meaningful names in shoot method

--- src/main/java/com/chrosciu/Shooter.java
+++ src/main/java/com/chrosciu/Shooter.java
@@ -39,33 +39,35 @@ public class Shooter {
         //iterate through all ships
         for (int i = 0; i < shipsWithHitMarks.size() && MISSED == result; ++i) {
             //iterate through all ship fields
-            for (int j = 0; j < shipsWithHitMarks.get(i).size() && MISSED == result; ++j) {
+            List<FieldWithHitMark> shipWithHitMarks = shipsWithHitMarks.get(i);
+            for (int j = 0; j < shipWithHitMarks.size() && MISSED == result; ++j) {
                 //if any of ship fields is equal to passed field - mark as hit
-                if (shipsWithHitMarks.get(i).get(j).getField().getX() == s.getX() && shipsWithHitMarks.get(i).get(j).getField().getY() == s.getY()) {
-                    shipsWithHitMarks.get(i).get(j).markAsHit();
+                if (shipWithHitMarks.get(j).getField().getX() == s.getX() && shipWithHitMarks.get(j).getField().getY() == s.getY()) {
+                    shipWithHitMarks.get(j).markAsHit();
                     result = HIT;
                 }
             }
             //if ship is hit - check if it is sunk
             if (HIT == result) {
                 //iterate through all fields and check if they are all hit
-                boolean a = true;
-                for (int j = 0; j < shipsWithHitMarks.get(i).size() && a; ++j) {
-                    a &= shipsWithHitMarks.get(i).get(j).isHit();
+                boolean isSunk = true;
+                for (int j = 0; j < shipWithHitMarks.size() && isSunk; ++j) {
+                    isSunk &= shipWithHitMarks.get(j).isHit();
                 }
-                if (a) {
+                if (isSunk) {
                     result = Result.SUNK;
                 }
             }
         }
         //check if all ships are sunk
-        boolean a = true;
-        for (int i = 0; i < shipsWithHitMarks.size() && a; ++i) {
-            for (int j = 0; j < shipsWithHitMarks.get(i).size() && a; ++j) {
-                a &= shipsWithHitMarks.get(i).get(j).isHit();
+        boolean allShipsSunk = true;
+        for (int i = 0; i < shipsWithHitMarks.size() && allShipsSunk; ++i) {
+            List<FieldWithHitMark> shipWithHitMarks = shipsWithHitMarks.get(i);
+            for (int j = 0; j < shipWithHitMarks.size() && allShipsSunk; ++j) {
+                allShipsSunk &= shipWithHitMarks.get(j).isHit();
             }
         }
-        if (a) {
+        if (allShipsSunk) {
             result = FINISHED;
         }
         return result;
