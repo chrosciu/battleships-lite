3306bc48835347926e3f82d8d29df4e2f73c6c34
Marcin Chrost <mchrost82@gmail.com>
2023-12-01 10:37:43 +0000

    Added more tests

--- src/test/java/ShooterTest.java
+++ src/test/java/ShooterTest.java
@@ -1,23 +1,61 @@
 import com.chrosciu.Shooter;
 import com.chrosciu.Shooter.Point;
 import org.apache.commons.lang3.tuple.Triple;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static com.chrosciu.Shooter.point;
 import static org.junit.Assert.assertEquals;
 
 public class ShooterTest {
-    private static final Point FIELD_WITHOUT_SHIP = Shooter.point(1, 2);
-    private static final List<Triple<Point, Integer, Boolean>> NO_SHIPS = Arrays.asList();
+
+    private static final Point VERTICAL_TWO_FIELDS_SHIP_FIRST_FIELD = point(3, 4);
+    private static final Point VERTICAL_TWO_FIELDS_SHIP_SECOND_FIELD = point(3, 5);
+
+    private static final Point ONE_FIELD_SHIP_FIELD = point(7, 2);
+
+    private static final Point ANOTHER_ONE_FIELD_SHIP_FIELD = point(6, 5);
+
+    private static final Point HORIZONTAL_TWO_FIELDS_SHIP_FIRST_FIELD = point(1, 1);
+    private static final Point HORIZONTAL_TWO_FIELDS_SHIP_SECOND_FIELD = point(2, 1);
+
+    private static final Point FIELD_WITHOUT_SHIP = point(1, 2);
+    private static final Point ANOTHER_FIELD_WITHOUT_SHIP = point(4, 4);
+
+    private static final boolean VERTICAL = true;
+    private static final boolean HORIZONTAL = false;
+
+    private static final int ONE_FIELD_SHIP_SIZE = 1;
+    private static final int TWO_FIELDS_SHIP_SIZE = 2;
+
+    private static final Triple<Point, Integer, Boolean> VERTICAL_TWO_FIELDS_SHIP =
+            Triple.of(VERTICAL_TWO_FIELDS_SHIP_FIRST_FIELD, TWO_FIELDS_SHIP_SIZE, VERTICAL);
+    private static final Triple<Point, Integer, Boolean> ONE_FIELD_SHIP =
+            Triple.of(ONE_FIELD_SHIP_FIELD, ONE_FIELD_SHIP_SIZE, HORIZONTAL);
+    private static final Triple<Point, Integer, Boolean> ANOTHER_ONE_FIELD_SHIP =
+            Triple.of(ANOTHER_ONE_FIELD_SHIP_FIELD, ONE_FIELD_SHIP_SIZE, VERTICAL);
+    private static final Triple<Point, Integer, Boolean> HORIZONTAL_TWO_FIELDS_SHIP =
+            Triple.of(HORIZONTAL_TWO_FIELDS_SHIP_FIRST_FIELD, TWO_FIELDS_SHIP_SIZE, HORIZONTAL);
+
+    private static final List<Triple<Point, Integer, Boolean>> BOARD_WITH_NO_SHIPS =
+            Arrays.asList();
+    private static final List<Triple<Point, Integer, Boolean>> BOARD_WITH_ONE_SHIP =
+            Arrays.asList(VERTICAL_TWO_FIELDS_SHIP);
+    private static final List<Triple<Point, Integer, Boolean>> BOARD_WITH_FOUR_SHIPS =
+            Arrays.asList(VERTICAL_TWO_FIELDS_SHIP, ONE_FIELD_SHIP, ANOTHER_ONE_FIELD_SHIP, HORIZONTAL_TWO_FIELDS_SHIP);
+
+    private static final int MISSED = 0;
+    private static final int HIT = 1;
+    private static final int SUNK = 2;
     private static final int FINISHED = 3;
+
 
     @Test
     public void shouldReportFinishedOnShotIfNoShipsOnBoard() {
         //given
-        Shooter shooter = new Shooter(NO_SHIPS);
+        Shooter shooter = new Shooter(BOARD_WITH_NO_SHIPS);
 
         //when
         int result = shooter.shoot(FIELD_WITHOUT_SHIP);
@@ -26,5 +64,100 @@ public class ShooterTest {
         assertEquals(FINISHED, result);
     }
 
+    @Test
+    public void shouldReportProperStatesWhenShootingOnSingleShip() {
+        //given
+        Shooter shooter = new Shooter(BOARD_WITH_ONE_SHIP);
+
+        //when
+        int result = shooter.shoot(FIELD_WITHOUT_SHIP);
+
+        //then
+        assertEquals("Expected MISSED result when shooting into field with no ship", MISSED, result);
+
+        //when
+        result = shooter.shoot(VERTICAL_TWO_FIELDS_SHIP_FIRST_FIELD);
+
+        //then
+        assertEquals(HIT, result);
+
+        //when
+        result = shooter.shoot(VERTICAL_TWO_FIELDS_SHIP_SECOND_FIELD);
+
+        //then
+        assertEquals(FINISHED, result);
+    }
+
+    @Test
+    public void shouldProperlyCalculateShootResultForGivenSetofShipsOnBoard() {
+        //given
+        Shooter shooter = new Shooter(BOARD_WITH_FOUR_SHIPS);
+
+        //when
+        int result = shooter.shoot(FIELD_WITHOUT_SHIP);
+
+        //then
+        assertEquals(MISSED, result);
+
+        //when
+        result = shooter.shoot(VERTICAL_TWO_FIELDS_SHIP_FIRST_FIELD);
+
+        //then
+        assertEquals(HIT, result);
+
+        //when
+        result = shooter.shoot(ONE_FIELD_SHIP_FIELD);
+
+        //then
+        assertEquals(SUNK, result);
+
+        //when
+        result = shooter.shoot(VERTICAL_TWO_FIELDS_SHIP_FIRST_FIELD);
+
+        //then
+        assertEquals(HIT, result);
+
+        //when
+        result = shooter.shoot(VERTICAL_TWO_FIELDS_SHIP_SECOND_FIELD);
+
+        //then
+        assertEquals(SUNK, result);
+
+        //when
+        result = shooter.shoot(VERTICAL_TWO_FIELDS_SHIP_SECOND_FIELD);
+
+        //then
+        assertEquals(SUNK, result);
+
+        //when
+        result = shooter.shoot(ANOTHER_ONE_FIELD_SHIP_FIELD);
+
+        //then
+        assertEquals(SUNK, result);
+
+        //when
+        result = shooter.shoot(HORIZONTAL_TWO_FIELDS_SHIP_FIRST_FIELD);
+
+        //then
+        assertEquals(HIT, result);
+
+        //when
+        result = shooter.shoot(HORIZONTAL_TWO_FIELDS_SHIP_SECOND_FIELD);
+
+        //then
+        assertEquals(FINISHED, result);
+
+        //when
+        result = shooter.shoot(HORIZONTAL_TWO_FIELDS_SHIP_FIRST_FIELD);
+
+        //then
+        assertEquals(FINISHED, result);
+
+        //when
+        result = shooter.shoot(ANOTHER_FIELD_WITHOUT_SHIP);
+
+        //then
+        assertEquals(FINISHED, result);
+    }
 
 }
