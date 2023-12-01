b9de13b0d52c8888dc26de408b1d769453fce0ea
Marcin Chrost <mchrost82@gmail.com>
2023-12-01 12:38:19 +0000

    Used Lombok to reduce boilerplate code in Field class

--- src/main/java/com/chrosciu/Field.java
+++ src/main/java/com/chrosciu/Field.java
@@ -1,23 +1,17 @@
 package com.chrosciu;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
 public class Field {
     private final int x;
     private final int y;
 
-    private Field(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
     public static Field of(int x, int y) {
         return new Field(x, y);
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 }
