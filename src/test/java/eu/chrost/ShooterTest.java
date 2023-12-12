7d097f146953fdfada0c599f19553a8a2eb4ab61
Marcin Chrost <mchrost82@gmail.com>
2023-12-12 14:39:20 +0100

    Added first test

--- src/test/java/eu/chrost/ShooterTest.java
+++ src/test/java/eu/chrost/ShooterTest.java
@@ -1,10 +1,26 @@
 package eu.chrost;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.jupiter.api.Test;
 
-class ShooterTest {
-    @Test
-    void test() {
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ShooterTest {
+    private static final Shooter.Point FIELD_WITHOUT_SHIP = Shooter.point(1, 2);
+    private static final List<Triple<Shooter.Point, Integer, Boolean>> BOARD_WITHOUT_SHIPS = List.of();
+    private static final int FINISHED = 3;
+
+    @Test
+    public void shouldReportFinishedWhenShootingOnBoardWithoutShips() {
+        //given
+        Shooter shooter = new Shooter(BOARD_WITHOUT_SHIPS);
+
+        //when
+        int result = shooter.shoot(FIELD_WITHOUT_SHIP);
+
+        //then
+        assertThat(result).isEqualTo(FINISHED);
     }
 }
