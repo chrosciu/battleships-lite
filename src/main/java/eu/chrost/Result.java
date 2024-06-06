6e640b0eeb0920de8d607290237437bec40a46ce
Marcin Chrost <mchrost82@gmail.com>
2024-06-06 09:26:25 +0000

    Get rid of rank field in Result enum (only needed during refactor)

--- src/main/java/eu/chrost/Result.java
+++ src/main/java/eu/chrost/Result.java
@@ -5,20 +5,9 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 
-@RequiredArgsConstructor
-@Getter
 public enum Result {
-    MISSED(0),
-    HIT(1),
-    SUNK(2),
-    FINISHED(3);
-
-    private final int rank;
-
-    public static Result fromRank(int rank) {
-        return Arrays.stream(values())
-                .filter(result -> rank == result.rank)
-                .findFirst()
-                .get();
-    }
+    MISSED,
+    HIT,
+    SUNK,
+    FINISHED;
 }
