148e8a8946b307b8c85cbed05036c006c533ed9c
Marcin Chrost <mchrost82@gmail.com>
2024-07-11 11:19:51 +0200

    Point static constrcutor marked as deprecated

--- src/main/java/eu/chrost/Point.java
+++ src/main/java/eu/chrost/Point.java
@@ -4,6 +4,16 @@ public class Point {
     private final int x;
     private final int y;
 
+    /**
+     * Create new Point instance
+     *
+     * Deprecated - use all args constructor instead
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    @Deprecated
     public static Point point(int x, int y) {
         Point point = new Point(x, y);
         return point;
