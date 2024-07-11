b7143492f414ca224621d49b8a8796fcd5cf063f
Marcin Chrost <mchrost82@gmail.com>
2024-07-11 10:54:05 +0200

    Get rid of rank field in Result enum (only needed during refactor)

--- src/main/java/eu/chrost/Result.java
+++ src/main/java/eu/chrost/Result.java
@@ -1,23 +1,8 @@
 package eu.chrost;
 
 public enum Result {
-    MISSED(0),
-    HIT(1),
-    SUNK(2),
-    FINISHED(3);
-
-    private final int rank;
-
-    Result(int rank) {
-        this.rank = rank;
-    }
-
-    public static Result fromRank(int rank) {
-        for (Result result: values()) {
-            if (result.rank == rank) {
-                return result;
-            }
-        }
-        throw new IllegalArgumentException("Invalid rank: " + rank);
-    }
+    MISSED,
+    HIT,
+    SUNK,
+    FINISHED;
 }
