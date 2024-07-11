7bd3e8a9b4eea6053523a75ca1ee8c571f5e9e11
Marcin Chrost <mchrost82@gmail.com>
2024-07-11 11:22:24 +0200

    Get rid of Point static constructor

--- src/main/java/eu/chrost/Point.java
+++ src/main/java/eu/chrost/Point.java
@@ -4,21 +4,6 @@ public class Point {
     private final int x;
     private final int y;
 
-    /**
-     * Create new Point instance
-     *
-     * Deprecated - use all args constructor instead
-     *
-     * @param x
-     * @param y
-     * @return
-     */
-    @Deprecated
-    public static Point point(int x, int y) {
-        Point point = new Point(x, y);
-        return point;
-    }
-
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
