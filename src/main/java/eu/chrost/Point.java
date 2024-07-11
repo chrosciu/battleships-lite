253641cd252429592061ff5a0c3338c4191338a7
Marcin Chrost <mchrost82@gmail.com>
2024-07-11 11:10:39 +0200

    Point class fields made private

--- src/main/java/eu/chrost/Point.java
+++ src/main/java/eu/chrost/Point.java
@@ -1,13 +1,21 @@
 package eu.chrost;
 
 public class Point {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
     public static Point point(int x, int y) {
         Point point = new Point();
         point.x = x;
         point.y = y;
         return point;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
