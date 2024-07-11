43bdbbf325f489b92680cd4f0de03c0092ada01a
Marcin Chrost <mchrost82@gmail.com>
2024-07-11 10:51:50 +0200

    Replaced fromRank() calls with explicite Result values inside shoot() method in Shooter

--- src/main/java/eu/chrost/Shooter.java
+++ src/main/java/eu/chrost/Shooter.java
@@ -3,6 +3,11 @@ package eu.chrost;
 import java.util.ArrayList;
 import java.util.List;
 
+import static eu.chrost.Result.FINISHED;
+import static eu.chrost.Result.HIT;
+import static eu.chrost.Result.MISSED;
+import static eu.chrost.Result.SUNK;
+
 public class Shooter {
 
     public static class Point {
@@ -85,26 +90,26 @@ public class Shooter {
      * @return - shot result
      */
     public Result shoot(Point s) {
-        var result = Result.fromRank(0);
+        var result = MISSED;
         //iterate through all ships
-        for (int i = 0; i < data.size() && Result.fromRank(0) == result; ++i) {
+        for (int i = 0; i < data.size() && MISSED == result; ++i) {
             //iterate through all ship fields
-            for (int j = 0; j < data.get(i).size() && Result.fromRank(0) == result; ++j) {
+            for (int j = 0; j < data.get(i).size() && MISSED == result; ++j) {
                 //if any of ship fields is equal to passed field - mark as hit
                 if (data.get(i).get(j).p.x == s.x && data.get(i).get(j).p.y == s.y) {
                     data.get(i).get(j).h = true;
-                    result = Result.fromRank(1);
+                    result = HIT;
                 }
             }
             //if ship is hit - check if it is sunk
-            if (Result.fromRank(1) == result) {
+            if (HIT == result) {
                 //iterate through all fields and check if they are all hit
                 boolean a = true;
                 for (int j = 0; j < data.get(i).size() && a; ++j) {
                     a &= data.get(i).get(j).h;
                 }
                 if (a) {
-                    result = Result.fromRank(2);
+                    result = SUNK;
                 }
             }
         }
@@ -116,7 +121,7 @@ public class Shooter {
             }
         }
         if (a) {
-            result = Result.fromRank(3);
+            result = FINISHED;
         }
         return result;
     }
