116e8017383069872290f5b0ec981416a9a90952
Marcin Chrost <mchrost82@gmail.com>
2024-07-12 11:34:25 +0200

    Extracted private helper method in Shooter constructor

--- src/main/java/eu/chrost/Shooter.java
+++ src/main/java/eu/chrost/Shooter.java
@@ -17,15 +17,20 @@ public class Shooter {
      */
     public Shooter(List<ShipDefinition> shipDefinitions) {
         for (var shipDefinition : shipDefinitions) {
-            Ship ship = new Ship();
-            for (int j = 0; j < shipDefinition.length(); ++j) {
-                var firstField = shipDefinition.firstField();
-                var orientation = shipDefinition.orientation();
-                var shiftedField = firstField.shift(j, orientation);
-                ship.add(new ShipField(shiftedField));
-            }
+            Ship ship = buildShipFromDefinition(shipDefinition);
             ships.add(ship);
         }
+    }
+
+    private Ship buildShipFromDefinition(ShipDefinition shipDefinition) {
+        Ship ship = new Ship();
+        for (int j = 0; j < shipDefinition.length(); ++j) {
+            var firstField = shipDefinition.firstField();
+            var orientation = shipDefinition.orientation();
+            var shiftedField = firstField.shift(j, orientation);
+            ship.add(new ShipField(shiftedField));
+        }
+        return ship;
     }
 
     /**
