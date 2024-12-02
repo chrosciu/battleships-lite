10d141afa4044ece8fee7ffd1d77b46f9d0daca7
Marcin Chrost <mchrost82@gmail.com>
2024-12-02 11:26:20 +0100

    Point class changed to standalone

---
+++ src/main/java/eu/chrost/Point.java
@@ -0,0 +1,6 @@
+package eu.chrost;
+
+class Point {
+    public int x;
+    public int y;
+}
--- src/main/java/eu/chrost/Shooter.java
+++ src/main/java/eu/chrost/Shooter.java
@@ -11,11 +11,6 @@ import static eu.chrost.Result.SUNK;
 
 class Shooter {
 
-    public static class Point {
-        public int x;
-        public int y;
-    }
-
     public static Point point(int x, int y) {
         Point point = new Point();
         point.x = x;
