ac4c0b2a76fc046d24e52a54356aac9b91cf3ca6
Marcin Chrost <mchrost82@gmail.com>
2024-12-02 11:36:56 +0100

    Point class made immutable

--- src/main/java/eu/chrost/Point.java
+++ src/main/java/eu/chrost/Point.java
@@ -1,16 +1,16 @@
 package eu.chrost;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 class Point {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public static Point point(int x, int y) {
-        Point point = new Point();
-        point.x = x;
-        point.y = y;
+        Point point = new Point(x, y);
         return point;
     }
 }
